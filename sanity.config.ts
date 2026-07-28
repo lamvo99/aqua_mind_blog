b08d927ea319fa7c0d25dae1ee1774c2dab25d04
lamvo99 <lamdevmobile@gmail.com>
2026-07-28 15:36:52 +0700

    make update santity studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
+import { codeInput } from '@sanity/code-input'
 import { schemaTypes } from './sanity/schemaTypes'
 import { structure } from './sanity/structure'
 import { apiVersion, dataset, projectId } from './sanity/env'
@@ -12,6 +13,7 @@ export default defineConfig({
   plugins: [
     structureTool({ structure }),
     visionTool({ defaultApiVersion: apiVersion }),
+    codeInput(),
   ],
   schema: {
     types: schemaTypes,
